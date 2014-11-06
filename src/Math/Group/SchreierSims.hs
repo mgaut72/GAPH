ca5f2de7f430338e6db246131ec27cf157fb394f
Matt Gautreau <mgaut72@gmail.com>
2014-11-06 13:09:50 -0700

    asdfasdfasdfa

---
+++ src/Math/Group/SchreierSims.hs
@@ -0,0 +1,9 @@
+module Math.Group.SchreierSims where
+
+import qualified Data.Set as S
+
+-- schreierSims (b,s)
+--   | S.null s  = (b,s)
+--   | otherwise = 
+--  where (c,t) = schreierSims (tail b, 
+
