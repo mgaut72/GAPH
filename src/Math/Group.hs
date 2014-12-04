1b9a9ce94d5dd588fb4bc30bfaed4a7d56b8f82e
Matt Gautreau <mgaut72@gmail.com>
2014-12-04 10:54:29 -0700

    add a Group and GroupAction typeclass, instances for permutations and matrices

---
+++ src/Math/Group.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Math.Group where
+
+import Data.Maybe (fromJust)
+
+import qualified Data.Set as S
+import qualified Math.Algebra.LinearAlgebra as L
+
+import qualified Math.Group.Permutation as P
+import qualified Math.Group.Matrix as M
+
+class Group a where
+  identity :: a
+  inverse  :: a -> a
+  times    :: a -> a -> a
+
+instance Ord a => Group (P.Permutation a) where
+  identity = P.zero
+  inverse  = P.negate
+  times    = P.compose
+
+instance (Eq a, Num a, Fractional a) => Group (M.Matrix a) where
+  identity = undefined -- how do I determine the size?
+  inverse  = fromJust . L.inverse  -- assumes matrix was invertible
+  times    = (L.<<*>>)
+
+
+-- Group consisting of elements of type a acting on a set of elements of type b
+class Group a => GroupAction a b where
+  action :: a -> b -> b
+
+instance Ord a => GroupAction (P.Permutation a) a where
+  action = P.lookup
+
+instance (Eq a, Num a, Fractional a) => GroupAction (M.Matrix a) (M.Vector a) where
+  action = (L.<<*>)
